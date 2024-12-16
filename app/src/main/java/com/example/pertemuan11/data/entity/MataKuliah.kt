dd54a952f907df6c8e1b599bcc594769cae93d50
Bayhaqiindra <bayhaqi30072004.com>
2024-12-16 16:54:37 +0700

    Tambah entitas MataKuliah dengan anotasi Room

---
+++ app/src/main/java/com/example/pertemuan11/data/entity/MataKuliah.kt
@@ -0,0 +1,14 @@
+package com.example.pertemuan11.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "mata_kuliah")
+data class MataKuliah(
+    @PrimaryKey val kode: String,
+    val nama: String,
+    val sks: Int,
+    val semester: Int,
+    val jenis: String,
+    val dosenPengampu: String // Foreign key reference
+)
