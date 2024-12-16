3633aec98cf8b1cffceaf3e83b70558792c4d6e0
Bayhaqiindra <bayhaqi30072004.com>
2024-12-16 17:39:22 +0700

    Menambahkan Interface MataKuliahDao dan menambahkan code Operasi CRUD

--- app/src/main/java/com/example/pertemuan11/data/dao/MataKuliahDao.kt
+++ app/src/main/java/com/example/pertemuan11/data/dao/MataKuliahDao.kt
@@ -1,4 +1,30 @@
 package com.example.pertemuan11.data.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.pertemuan11.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface MataKuliahDao {
+    @Insert
+    suspend fun insertMataKuliah(
+        mataKuliah: MataKuliah
+    )
+
+    @Query("SELECT * FROM mata_kuliah ORDER BY nama ASC")
+    fun getAllMataKuliah () : Flow<List<MataKuliah>>
+
+    @Query("SELECT * FROM mata_kuliah WHERE kode = :kode")
+    fun getMataKuliah (kode: String) : Flow<MataKuliah>
+
+    @Delete
+    suspend fun deleteMataKuliah (mataKuliah: MataKuliah)
+
+    @Update
+    suspend fun updateMataKuliah (mataKuliah: MataKuliah)
+
 }
