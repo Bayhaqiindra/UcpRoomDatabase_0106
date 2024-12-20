7842a519f956c44e65de8e3c7ff8b6e95e484189
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 17:13:33 +0700

    `Tambah HomeMataKuliahViewModel untuk mengelola data mata kuliah dengan alur state`

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/HomeMataKuliahViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/HomeMataKuliahViewModel.kt
@@ -1,6 +1,50 @@
 package com.example.pertemuan11.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan11.data.entity.MataKuliah
+import com.example.pertemuan11.repository.RepositoryMk
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeMataKuliahViewModel (
+    private val repositoryMk: RepositoryMk
+) : ViewModel() {
+    val homeMatakuliahUiState: StateFlow<HomeMatakuliahUiState> = repositoryMk.getAllMk()
+        .filterNotNull()
+        .map {
+            HomeMatakuliahUiState (
+                listMtk = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeMatakuliahUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeMatakuliahUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeMatakuliahUiState(
+                isLoading = true,
+            )
+        )
+}
 
 data class HomeMatakuliahUiState (
     val listMtk: List<MataKuliah> = listOf(),
