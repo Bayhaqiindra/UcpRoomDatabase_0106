c61877fa07f92d9b708bafa26533bc579b3dabc9
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 20:49:59 +0700

    Menambahkan form input untuk matakuliah

--- app/src/main/java/com/example/pertemuan11/ui/view/matakuliah/InsertMataKuliah.kt
+++ app/src/main/java/com/example/pertemuan11/ui/view/matakuliah/InsertMataKuliah.kt
@@ -1,7 +1,21 @@
 package com.example.pertemuan11.ui.view.matakuliah
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.example.pertemuan11.data.entity.Dosen
 import com.example.pertemuan11.ui.viewmodel.matakuliah.FormErrorState
 import com.example.pertemuan11.ui.viewmodel.matakuliah.MatakuliahEvent
@@ -14,5 +28,116 @@ fun FormMataKuliah(
     modifier: Modifier = Modifier,
     listDsn: List<Dosen>
 ) {
+    val semester = listOf("Ganjil", "Genap")
+    val jenis = listOf("Programming", "Data", "Network", "UI/UX")
+    val namaDosenList = listDsn.map { it.nama }
 
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = matakuliahEvent.kode,
+            onValueChange = {
+                onValueChange(matakuliahEvent.copy(kode = it))
+            },
+            label = { Text("Kode") },
+            isError = errorState.kode != null,
+            placeholder = { Text("Masukkan Kode") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(
+            text = errorState.kode ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = matakuliahEvent.nama,
+            onValueChange = {
+                onValueChange(matakuliahEvent.copy(nama = it))
+            },
+            label = { Text("Nama") },
+            isError = errorState.nama != null,
+            placeholder = { Text("Masukkan Nama") },
+        )
+        Text(
+            text = errorState.nama ?: "",
+            color = Color.Red
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "Semester")
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            semester.forEach { semesterOption ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    RadioButton(
+                        selected = matakuliahEvent.semester == semesterOption,
+                        onClick = {
+                            onValueChange(matakuliahEvent.copy(jenis = semesterOption))
+                        },
+                    )
+                    Text(text = semesterOption)
+                }
+            }
+        }
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = matakuliahEvent.sks,
+            onValueChange = {
+                onValueChange(matakuliahEvent.copy(sks = it))
+            },
+            label = { Text("SKS") },
+            isError = errorState.sks != null,
+            placeholder = { Text("Masukkan SKS") },
+        )
+        Text(
+            text = errorState.sks ?: "",
+            color = Color.Red
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "Jenis")
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            jenis.forEach { jenisOption ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    RadioButton(
+                        selected = matakuliahEvent.jenis == jenisOption,
+                        onClick = {
+                            onValueChange(matakuliahEvent.copy(jenis = jenisOption))
+                        },
+                    )
+                    Text(text = jenisOption)
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "Dosen Pengampu")
+        DynamicSelectedTextField(
+            selectedValue = matakuliahEvent.dosenPengampu,
+            options = namaDosenList,
+            label = "Pilih Dosen Pengampu",
+            onValueChangedEvent = {
+                onValueChange(matakuliahEvent.copy(dosenPengampu = it))
+            }
+        )
+        Text(
+            text = errorState.dosenPengampu ?: "",
+            color = Color.Red
+        )
+    }
 }
