8bf6ad4fc7d39c1e6c2943f4410a74db7710c381
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 18:12:35 +0700

    `Gunakan ViewModel HomeDosenViewModel dengan factory dari PenyediaDosenViewModel`

--- app/src/main/java/com/example/pertemuan11/ui/view/dosen/HomeDosenView.kt
+++ app/src/main/java/com/example/pertemuan11/ui/view/dosen/HomeDosenView.kt
@@ -1,8 +1,13 @@
 package com.example.pertemuan11.ui.view.dosen
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan11.ui.viewmodel.dosen.HomeDosenViewModel
+import com.example.pertemuan11.ui.viewmodel.dosen.PenyediaDosenViewModel
 
 @Composable
-fun HomeDosenView(){
+fun HomeDosenView(
+    viewModel: HomeDosenViewModel = viewModel(factory = PenyediaDosenViewModel.Factory),
+){
 
 }
