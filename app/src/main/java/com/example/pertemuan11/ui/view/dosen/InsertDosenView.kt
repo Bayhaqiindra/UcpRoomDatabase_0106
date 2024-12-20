071701a1426e70277dbe21e9f03017ca071ccd5f
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 19:15:47 +0700

    Menambahkan fitur snackbar dan logika penyimpanan data dosen

--- app/src/main/java/com/example/pertemuan11/ui/view/dosen/InsertDosenView.kt
+++ app/src/main/java/com/example/pertemuan11/ui/view/dosen/InsertDosenView.kt
@@ -1,8 +1,81 @@
 package com.example.pertemuan11.ui.view.dosen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan11.ui.customwidget.TopAppBar
+import com.example.pertemuan11.ui.viewmodel.dosen.DosenViewModel
+import com.example.pertemuan11.ui.viewmodel.dosen.PenyediaDosenViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
-fun InsertDosenView(){
+fun InsertDosenView(
+    onBack:()->Unit,
+    onNavigate:()->Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DosenViewModel = viewModel(factory = PenyediaDosenViewModel.Factory)
+){
+    val uiState = viewModel.uiState
+    val snackbarHostState = remember{ SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
 
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message)
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Dosen",
+                modifier = modifier
+            )
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            InsertBodyDosen(
+                uiState = uiState,
+                onValueChange = { updateEvent ->
+                    viewModel.updateState(updateEvent)
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.validateFields()) {
+                            viewModel.saveData()
+                            delay(500)
+                            withContext(Dispatchers.Main) {
+                                onNavigate()
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
 }
