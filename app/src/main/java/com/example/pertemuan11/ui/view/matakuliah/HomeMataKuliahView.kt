07b6ec2fb9f047d6d6753ae51a9e9d642145b641
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 19:30:47 +0700

    Membuat Function CardMataKuliah

---
+++ app/src/main/java/com/example/pertemuan11/ui/view/matakuliah/HomeMataKuliahView.kt
@@ -0,0 +1,14 @@
+package com.example.pertemuan11.ui.view.matakuliah
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pertemuan11.data.entity.MataKuliah
+
+@Composable
+fun CardMataKuliah(
+    mtk: MataKuliah,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+){
+
+}
