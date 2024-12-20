a8c368358673b65bda6d4a4c05cc2ba53b4eae78
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 14:40:35 +0700

    `Implementasi ContainerApp untuk menyediakan instance RepositoryDosen dan RepositoryMk`

--- app/src/main/java/com/example/pertemuan11/dependeciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan11/dependeciesinjection/ContainerApp.kt
@@ -1,9 +1,23 @@
 package com.example.pertemuan11.dependeciesinjection
 
+import android.content.Context
+import com.example.pertemuan11.data.database.KrsDatabase
+import com.example.pertemuan11.repository.LocalRepositoryDosen
+import com.example.pertemuan11.repository.LocalRepositoryMk
 import com.example.pertemuan11.repository.RepositoryDosen
 import com.example.pertemuan11.repository.RepositoryMk
 
 interface InterfaceContainerApp {
     val repositoryDosen: RepositoryDosen
     val repositoryMk: RepositoryMk
+}
+
+class ContainerApp(private val context: Context) : InterfaceContainerApp
+{
+    override val repositoryDosen: RepositoryDosen by lazy {
+        LocalRepositoryDosen(KrsDatabase.getDataBase(context).dosenDao())
+    }
+    override val repositoryMk: RepositoryMk by lazy {
+        LocalRepositoryMk(KrsDatabase.getDataBase(context).matakuliahDao())
+    }
 }
