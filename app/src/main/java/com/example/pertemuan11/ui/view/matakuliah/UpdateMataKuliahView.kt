3c2178a46acd8a6a7f90b8ec6c4e80f162a95afa
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 21:03:25 +0700

    Menambahkan fitur edit matakuliah dengan validasi input dan snackbar.

--- app/src/main/java/com/example/pertemuan11/ui/view/matakuliah/UpdateMataKuliahView.kt
+++ app/src/main/java/com/example/pertemuan11/ui/view/matakuliah/UpdateMataKuliahView.kt
@@ -1,11 +1,30 @@
 package com.example.pertemuan11.ui.view.matakuliah
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan11.ui.customwidget.TopAppBar
 import com.example.pertemuan11.ui.viewmodel.dosen.HomeDosenViewModel
 import com.example.pertemuan11.ui.viewmodel.dosen.PenyediaDosenViewModel
 import com.example.pertemuan11.ui.viewmodel.matakuliah.PenyediaMkViewModel
 import com.example.pertemuan11.ui.viewmodel.matakuliah.UpdateMataKuliahViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun UpdateMataKuliahView(
@@ -14,6 +33,62 @@ fun UpdateMataKuliahView(
     modifier: Modifier = Modifier,
     viewModel: UpdateMataKuliahViewModel = viewModel(factory = PenyediaMkViewModel.Factory),
     viewModelDsn: HomeDosenViewModel = viewModel(factory = PenyediaDosenViewModel.Factory),
-){
+) {
+    val uiState = viewModel.updateUIState
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+    val DsnList by viewModelDsn.homeUiState.collectAsState()
 
+    LaunchedEffect(uiState.snackBarMessage) {
+        println("LaunchedEffect triggered")
+        uiState.snackBarMessage?.let { message ->
+            println("Snackbar message received: $message")
+            coroutineScope.launch {
+                println("Launching coroutine for snacbar")
+                snackbarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Long
+                )
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                judul = "Edit Matakuliah",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = Modifier
+            )
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            InsertBodyMataKuliah(
+                uiState = uiState,
+                listDosen = DsnList,
+                onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent)
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.validateFields()) {
+                            viewModel.updateData()
+                            delay(600)
+                            withContext(Dispatchers.Main) {
+                                onNavigate()
+                            }
+                        }
+                    }
+                },
+            )
+        }
+    }
 }
