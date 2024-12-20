09d040cfa5f55a500c1c6f8229a3dd5e56c7b04e
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 15:33:13 +0700

    `Tambah objek PenyediaDosenViewModel untuk menyediakan ViewModel dengan dependensi`

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/dosen/PenyediaDosenViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/dosen/PenyediaDosenViewModel.kt
@@ -2,7 +2,24 @@ package com.example.pertemuan11.ui.viewmodel.dosen
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pertemuan11.KrsApp
 
-fun CreationExtras.krsApp(): KrsApp =
+object PenyediaDosenViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            DosenViewModel(
+                KrsApp().containerApp.repositoryDosen,
+            )
+        }
+        initializer {
+            HomeDosenViewModel(
+                KrsApp().containerApp.repositoryDosen,
+            )
+        }
+    }
+}
+
+fun CreationExtras.KrsApp(): KrsApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
