47fcf76df4e0fe7abc3b8ce2ed587647ec2517da
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 14:36:18 +0700

    `Tambah InterfaceContainerApp untuk menyediakan dependensi RepositoryDosen dan RepositoryMk`

--- app/src/main/java/com/example/pertemuan11/dependeciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan11/dependeciesinjection/ContainerApp.kt
@@ -1,2 +1,9 @@
 package com.example.pertemuan11.dependeciesinjection
 
+import com.example.pertemuan11.repository.RepositoryDosen
+import com.example.pertemuan11.repository.RepositoryMk
+
+interface InterfaceContainerApp {
+    val repositoryDosen: RepositoryDosen
+    val repositoryMk: RepositoryMk
+}
