137ff4a1ee3b70a6c3f6afe210ffdc248fc5999d
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 14:09:03 +0700

    Menambahkan Suspend Untuk operasi berat create,read,update,delete

--- app/src/main/java/com/example/pertemuan11/repository/RepositoryMk.kt
+++ app/src/main/java/com/example/pertemuan11/repository/RepositoryMk.kt
@@ -1,4 +1,16 @@
 package com.example.pertemuan11.repository
 
+import com.example.pertemuan11.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoryMk {
+    suspend fun insertMk(mataKuliah: MataKuliah)
+
+    fun getAllMk(): Flow<List<MataKuliah>>
+
+    fun getMk(kode: String): Flow<MataKuliah>
+
+    suspend fun updateMk(mataKuliah: MataKuliah)
+
+    suspend fun deleteMk(mataKuliah: MataKuliah)
 }
