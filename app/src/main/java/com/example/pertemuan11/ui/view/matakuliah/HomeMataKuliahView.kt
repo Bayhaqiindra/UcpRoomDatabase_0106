c11acf898738771b2f4ffd06d6ed1f936523cc4e
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 19:41:15 +0700

    Tambah CardMataKuliah untuk detail mata kuliah.

--- app/src/main/java/com/example/pertemuan11/ui/view/matakuliah/HomeMataKuliahView.kt
+++ app/src/main/java/com/example/pertemuan11/ui/view/matakuliah/HomeMataKuliahView.kt
@@ -1,14 +1,122 @@
 package com.example.pertemuan11.ui.view.matakuliah
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pertemuan11.data.entity.MataKuliah
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CardMataKuliah(
     mtk: MataKuliah,
     modifier: Modifier = Modifier,
     onClick: () -> Unit = { }
 ){
-
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ){
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ){
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Info, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mtk.kode,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row (
+                modifier = Modifier. fillMaxWidth (),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                    Spacer(modifier = Modifier.padding(4.dp))
+                    Text(
+                        text = mtk.nama,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(imageVector = Icons.Filled.Create, contentDescription = "")
+                    Spacer(modifier = Modifier.padding(4.dp))
+                    Text(
+                        text = mtk.semester,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Icon(imageVector = Icons.Filled.DateRange, contentDescription = "")
+                    Spacer(modifier = Modifier.padding(4.dp))
+                    Text(
+                        text = mtk.sks,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(imageVector = Icons.Filled.CheckCircle, contentDescription = "")
+                    Spacer(modifier = Modifier.padding(4.dp))
+                    Text(
+                        text = mtk.jenis,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(imageVector = Icons.Filled.AccountBox, contentDescription = "")
+                    Spacer(modifier = Modifier.padding(4.dp))
+                    Text(
+                        text = mtk.dosenPengampu,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+            }
+        }
+    }
 }
