6ee5a315231108485e0bce0c2d31ef8f7673511a
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 20:40:02 +0700

    Membuat Function FormMataKuliah

--- app/src/main/java/com/example/pertemuan11/ui/view/matakuliah/InsertMataKuliah.kt
+++ app/src/main/java/com/example/pertemuan11/ui/view/matakuliah/InsertMataKuliah.kt
@@ -1,2 +1,18 @@
 package com.example.pertemuan11.ui.view.matakuliah
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pertemuan11.data.entity.Dosen
+import com.example.pertemuan11.ui.viewmodel.matakuliah.FormErrorState
+import com.example.pertemuan11.ui.viewmodel.matakuliah.MatakuliahEvent
+
+@Composable
+fun FormMataKuliah(
+    matakuliahEvent: MatakuliahEvent = MatakuliahEvent(),
+    onValueChange: (MatakuliahEvent) -> Unit = {},
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier,
+    listDsn: List<Dosen>
+) {
+
+}
