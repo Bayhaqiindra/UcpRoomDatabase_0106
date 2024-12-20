3553c069db34724019de011c6a30727b1bbca8a7
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 21:22:17 +0700

    Membuat function DetailMataKuliahView

---
+++ app/src/main/java/com/example/pertemuan11/ui/view/matakuliah/DetailMataKuliahView.kt
@@ -0,0 +1,16 @@
+package com.example.pertemuan11.ui.view.matakuliah
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pertemuan11.data.entity.MataKuliah
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardMatakuliah(
+    mtk: MataKuliah,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+
+}
