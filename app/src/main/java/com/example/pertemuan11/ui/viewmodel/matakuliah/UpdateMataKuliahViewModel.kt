728f39b92c3a8c4ebd556a904430aeae440d5877
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 18:00:04 +0700

    `Inisialisasi updateUIState dengan data mata kuliah berdasarkan kode`

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/UpdateMataKuliahViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/UpdateMataKuliahViewModel.kt
@@ -5,8 +5,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan11.data.entity.MataKuliah
 import com.example.pertemuan11.repository.RepositoryMk
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class UpdateMataKuliahViewModel(
     savedStateHandle: SavedStateHandle,
@@ -15,6 +19,23 @@ class UpdateMataKuliahViewModel(
 
     var updateUIState by mutableStateOf(MatakuliahUIState())
         private set
+
+    private val _kode: String = checkNotNull(savedStateHandle[DestinasiMatakuliahUpdate.KODE])
+
+    init {
+        viewModelScope.launch {
+            updateUIState = repositoryMk.getMk(_kode)
+                .filterNotNull()
+                .first()
+                .toUiStateMatakuliah()
+        }
+    }
+
+    fun updateState (matakuliahEvent: MatakuliahEvent) {
+        updateUIState = updateUIState.copy(
+            matakuliahEvent = matakuliahEvent,
+        )
+    }
 }
 fun MataKuliah.toUiStateMatakuliah () : MatakuliahUIState = MatakuliahUIState(
     matakuliahEvent = this.toDetailMatakuliahUiEvent(),
