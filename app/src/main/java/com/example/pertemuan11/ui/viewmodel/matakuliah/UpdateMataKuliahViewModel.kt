d510e6ee71466499e437311748b5671765102350
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 17:57:46 +0700

    `Tambah UpdateMataKuliahViewModel untuk mengelola proses pembaruan data mata kuliah`

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/UpdateMataKuliahViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/UpdateMataKuliahViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.pertemuan11.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.pertemuan11.data.entity.MataKuliah
+import com.example.pertemuan11.repository.RepositoryMk
 
+class UpdateMataKuliahViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMk: RepositoryMk
+) : ViewModel() {
+
+}
 fun MataKuliah.toUiStateMatakuliah () : MatakuliahUIState = MatakuliahUIState(
     matakuliahEvent = this.toDetailMatakuliahUiEvent(),
 )
