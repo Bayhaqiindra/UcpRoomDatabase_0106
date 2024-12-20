767d04628a7a22a1464904e1029c8a5860d41f40
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 15:38:53 +0700

    `Tambah ekstensi fungsi untuk mengonversi MatakuliahEvent ke MataKuliahEntity`

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
@@ -1,6 +1,17 @@
 package com.example.pertemuan11.ui.viewmodel.matakuliah
 
-data class MataKuliahEvent(
+import com.example.pertemuan11.data.entity.MataKuliah
+
+fun MatakuliahEvent.toMatakuliahEntity(): MataKuliah = MataKuliah(
+    kode = kode,
+    nama = nama,
+    sks = sks,
+    semester = semester,
+    jenis = jenis,
+    dosenPengampu = dosenPengampu
+)
+
+data class MatakuliahEvent(
     val kode: String = "",
     val nama: String = "",
     val sks: String = "",
