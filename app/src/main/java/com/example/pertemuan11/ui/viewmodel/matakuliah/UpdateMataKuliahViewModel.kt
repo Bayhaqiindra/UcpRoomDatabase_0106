b753b13b551926d648b38ad4af61536714979838
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 17:42:45 +0700

    Perbaikan Salah Penempatan pada code

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/UpdateMataKuliahViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/UpdateMataKuliahViewModel.kt
@@ -1,7 +1,1 @@
 package com.example.pertemuan11.ui.viewmodel.matakuliah
-
-import com.example.pertemuan11.data.entity.MataKuliah
-
-fun MataKuliah.toUiStateMatakuliah () : MatakuliahUIState = MatakuliahUIState(
-    matakuliahEvent = this.toDetailMatakuliahUiEvent(),
-)
