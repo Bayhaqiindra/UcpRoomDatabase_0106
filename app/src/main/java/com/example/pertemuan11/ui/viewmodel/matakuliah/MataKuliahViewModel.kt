7348edfded4eefd46c75f26e53bce3d12b8412af
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 15:36:08 +0700

    `Tambah data class MataKuliahEvent untuk menyimpan data mata kuliah`

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.pertemuan11.ui.viewmodel.matakuliah
 
+data class MataKuliahEvent(
+    val kode: String = "",
+    val nama: String = "",
+    val sks: String = "",
+    val semester: String = "",
+    val jenis: String = "",
+    val dosenPengampu: String = ""
+)
