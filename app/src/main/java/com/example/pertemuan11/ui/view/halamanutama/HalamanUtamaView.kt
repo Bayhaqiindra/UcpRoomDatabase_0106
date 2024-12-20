b9463f588c736a42096704bbb383cbb1abe788cb
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 21:56:26 +0700

    membuat HalamanUtamaView dan membuat functionnya

---
+++ app/src/main/java/com/example/pertemuan11/ui/view/halamanutama/HalamanUtamaView.kt
@@ -0,0 +1,13 @@
+package com.example.pertemuan11.ui.view.halamanutama
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HalamanUtamaView(
+    onDosenClick: () -> Unit,
+    onMataKuliahClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+}
