aacd94c185b67a6ca71d17742d84754b9731a0a6
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 14:05:54 +0700

    Tambah interface RepositoryDosen dengan metode insert dan getAll

--- app/src/main/java/com/example/pertemuan11/repository/RepositoryDosen.kt
+++ app/src/main/java/com/example/pertemuan11/repository/RepositoryDosen.kt
@@ -1,4 +1,9 @@
 package com.example.pertemuan11.repository
 
+import com.example.pertemuan11.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoryDosen {
+    suspend fun insertDsn(dosen: Dosen)
+    fun getAllDsn(): Flow<List<Dosen>>
 }
