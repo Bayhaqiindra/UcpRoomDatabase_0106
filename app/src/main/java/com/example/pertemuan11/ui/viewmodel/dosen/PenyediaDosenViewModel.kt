9db85501d16bf2e3995093ecb900dd79d1b5a490
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 15:29:04 +0700

    `Tambah ekstensi CreationExtras untuk mendapatkan instance KrsApp`

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/dosen/PenyediaDosenViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/dosen/PenyediaDosenViewModel.kt
@@ -1,2 +1,8 @@
 package com.example.pertemuan11.ui.viewmodel.dosen
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.pertemuan11.KrsApp
+
+fun CreationExtras.krsApp(): KrsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
