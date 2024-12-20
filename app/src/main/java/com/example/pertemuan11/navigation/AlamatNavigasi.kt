615515b29fdf98e098992ee159a43b5c6e928f69
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 23:05:37 +0700

    Tambah objek navigasi untuk berbagai destinasi halaman

--- app/src/main/java/com/example/pertemuan11/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pertemuan11/navigation/AlamatNavigasi.kt
@@ -1,2 +1,31 @@
 package com.example.pertemuan11.navigation
 
+
+interface AlamatNavigasi {
+    val route: String
+}
+
+object DestinasiHalamanUtama : AlamatNavigasi {
+    override val route = "halamanutama"
+}
+
+object DestinasiDosen : AlamatNavigasi {
+    override val route = "dosen"
+}
+
+
+object DestinasiMataKuliah : AlamatNavigasi {
+    override val route = "matakuliah"
+}
+
+object DestinasiMataKuliahDetail : AlamatNavigasi {
+    override val route = "matakuliahdetail"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
+
+object DestinasiMataKuliahUpdate : AlamatNavigasi {
+    override val route = "matakuliahupdate"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
