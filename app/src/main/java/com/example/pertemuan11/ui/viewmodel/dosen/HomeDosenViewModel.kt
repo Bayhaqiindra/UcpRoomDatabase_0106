2a3c84e76a2064d0720fb591cad0aff31f0b9058
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 15:07:12 +0700

    Implementasi HomeDosenViewModel untuk mengelola StateFlow UI terkait data Dosen

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/dosen/HomeDosenViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/dosen/HomeDosenViewModel.kt
@@ -1,13 +1,49 @@
 package com.example.pertemuan11.ui.viewmodel.dosen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan11.data.entity.Dosen
 import com.example.pertemuan11.repository.RepositoryDosen
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeDosenViewModel (
     private val repositoryDosen: RepositoryDosen
 ) : ViewModel() {
-
+    val homeUiState: StateFlow<HomeUiState> = repositoryDosen.getAllDsn()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listDsn = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
 }
 
 data class HomeUiState(
