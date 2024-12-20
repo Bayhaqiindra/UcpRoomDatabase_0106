aca7d917cb1ebb44a4e4e24c6931cc272fe131b4
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 15:02:27 +0700

    `Tambahkan HomeDosenViewModel untuk mengelola data dan logika terkait Dosen`

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/dosen/HomeDosenViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/dosen/HomeDosenViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.pertemuan11.ui.viewmodel.dosen
 
+import androidx.lifecycle.ViewModel
 import com.example.pertemuan11.data.entity.Dosen
+import com.example.pertemuan11.repository.RepositoryDosen
+
+class HomeDosenViewModel (
+    private val repositoryDosen: RepositoryDosen
+) : ViewModel() {
+
+}
 
 data class HomeUiState(
     val listDsn: List<Dosen> = listOf(),
