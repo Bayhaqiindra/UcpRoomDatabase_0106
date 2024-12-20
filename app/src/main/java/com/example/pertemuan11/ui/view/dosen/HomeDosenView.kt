924c5f2539d8a747bcbbf29c01e3c5208d4fe17e
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 18:13:10 +0700

    `Tambah parameter onAddDosen, onBack, dan onDetailClick untuk navigasi dan interaksi di komponen`

--- app/src/main/java/com/example/pertemuan11/ui/view/dosen/HomeDosenView.kt
+++ app/src/main/java/com/example/pertemuan11/ui/view/dosen/HomeDosenView.kt
@@ -1,6 +1,7 @@
 package com.example.pertemuan11.ui.view.dosen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan11.ui.viewmodel.dosen.HomeDosenViewModel
 import com.example.pertemuan11.ui.viewmodel.dosen.PenyediaDosenViewModel
@@ -8,6 +9,10 @@ import com.example.pertemuan11.ui.viewmodel.dosen.PenyediaDosenViewModel
 @Composable
 fun HomeDosenView(
     viewModel: HomeDosenViewModel = viewModel(factory = PenyediaDosenViewModel.Factory),
+    onAddDosen: () -> Unit = {},
+    onBack: () -> Unit,
+    onDetailClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
 ){
 
 }
