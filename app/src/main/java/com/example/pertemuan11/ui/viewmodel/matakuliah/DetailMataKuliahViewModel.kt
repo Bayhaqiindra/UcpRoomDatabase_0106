0b88bf3ee5ddfc96bb3ce28b64b4ec638120b2d8
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 17:26:26 +0700

    `Tambah data class DetailMatakuliahUiState untuk mengelola state detail mata kuliah`

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/DetailMataKuliahViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/DetailMataKuliahViewModel.kt
@@ -2,6 +2,19 @@ package com.example.pertemuan11.ui.viewmodel.matakuliah
 
 import com.example.pertemuan11.data.entity.MataKuliah
 
+data class DetailMatakuliahUiState(
+    val detailMatakuliahUiEvent: MatakuliahEvent = MatakuliahEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailMatakuliahUiEvent == MatakuliahEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailMatakuliahUiEvent != MatakuliahEvent()
+}
+
 fun MataKuliah.toDetailMatakuliahUiEvent(): MatakuliahEvent {
     return MatakuliahEvent(
         kode = kode,
