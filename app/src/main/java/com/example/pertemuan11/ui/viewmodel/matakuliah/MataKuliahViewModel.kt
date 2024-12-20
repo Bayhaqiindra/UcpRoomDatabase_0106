b2b5866909601a228f57c39275c0505e9addaaaf
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 15:43:13 +0700

    `Tambah MataKuliahViewModel untuk mengelola state dan logika bisnis mata kuliah`

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.pertemuan11.ui.viewmodel.matakuliah
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.pertemuan11.data.entity.MataKuliah
+import com.example.pertemuan11.repository.RepositoryMk
+
+class MataKuliahViewModel(private val repositoryMk: RepositoryMk): ViewModel() {
+    var uiState by mutableStateOf(MatakuliahUIState())
+
 
 data class MatakuliahUIState(
     val matakuliahEvent: MatakuliahEvent = MatakuliahEvent(),
