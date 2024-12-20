d1feb49861acbfb11d54410b2397ad4117b6ec4f
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 17:47:47 +0700

    `Tambah ViewModelFactory untuk menyediakan MataKuliahViewModel, HomeMataKuliahViewModel, dan DetailMataKuliahViewModel`

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/PenyediaMkViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/matakuliah/PenyediaMkViewModel.kt
@@ -1,8 +1,33 @@
 package com.example.pertemuan11.ui.viewmodel.matakuliah
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pertemuan11.KrsApp
+
+object PenyediaMkViewModel {
+
+    val Factory = viewModelFactory {
+        initializer {
+            MataKuliahViewModel(
+                KrsApp().containerApp.repositoryMk
+            )
+        }
+        initializer {
+            HomeMataKuliahViewModel(
+                KrsApp().containerApp.repositoryMk,
+            )
+        }
+        initializer {
+            DetailMataKuliahViewModel(
+                createSavedStateHandle(),
+                KrsApp().containerApp.repositoryMk,
+            )
+        }
+    }
+}
 
 fun CreationExtras.KrsApp(): KrsApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
