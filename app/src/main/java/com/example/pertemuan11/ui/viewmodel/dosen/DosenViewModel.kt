82ffef11ca04a73d75c4ac7016cb0e677cf65720
Bayhaqiindra <bayhaqi30072004.com>
2024-12-20 15:11:06 +0700

    `Tambahkan fungsi ekstensi toDosenEntity untuk mengonversi DosenEvent menjadi entitas Dosen`

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/dosen/DosenViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/dosen/DosenViewModel.kt
@@ -1,5 +1,13 @@
 package com.example.pertemuan11.ui.viewmodel.dosen
 
+import com.example.pertemuan11.data.entity.Dosen
+
+fun DosenEvent.toDosenEntity(): Dosen = Dosen(
+    nidn = nidn,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+)
+
 data class DosenEvent(
     val nidn: String = "",
     val nama: String ="",
